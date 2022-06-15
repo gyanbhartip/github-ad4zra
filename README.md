7e44784328690a25d4cc71d4b944940640ac5ed1
gyanbhartip <gyanbhartip@gmail.com>
2022-06-15 07:33:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ad4zra
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ad4zra)
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { useState } from 'react';
+import './App.css';
+import NewTaskField from './components/NewTaskField';
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+
+  return (
+    <div className="app">
+      <span className="heading">typescript-todo</span>
+      <NewTaskField todo={todo} setTodo={setTodo} />
+    </div>
+  );
+};
+
+export default App;
